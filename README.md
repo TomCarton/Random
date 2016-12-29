391a641ec98c8286c9d909493e07b06eddf92480
tom <thomas.carton@23h23.com>
2016-12-30 00:09:04 +0100

    added result folder with resulting images

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Rendering
 Some random rendering code made from scratch to ensure reflexes are still there ;)
+
+**Mandelbrot**
+
+![](https://github.com/TomCarton/Rendering/blob/master/result/mandelbrot.png)
+
+**Flag**
+
+![](https://github.com/TomCarton/Rendering/blob/master/result/flag.png)
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include "tools/image.h"
 #include "render/renderFlag.h"
+#include "render/renderMandelbrot.h"
 
 
 static const unsigned int kWidth = 800;
@@ -13,10 +14,12 @@ static const unsigned int kHeight = 600;
 int main(int argc, char *argv[])
 {
         unsigned char image[kWidth * kHeight * 3];
-        RenderFrenchFlag(kWidth, kHeight, image);
 
-        SaveImageTGA("flag.tga", image, kWidth, kHeight);
-        SaveImagePPM("flag.ppm", image, kWidth, kHeight);
+        RenderFrenchFlag(kWidth, kHeight, image);
+        SaveImageTGA("result/flag.tga", image, kWidth, kHeight);
+
+        RenderMandelbrot(kWidth, kHeight, image);
+        SaveImageTGA("result/mandelbrot.tga", image, kWidth, kHeight);
 
         return 0;
 }
