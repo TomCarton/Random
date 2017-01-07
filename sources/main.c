dc0d225336df41c2f682640398eea2863cd6cb10
tom <thomas.carton@23h23.com>
2017-01-07 12:05:32 +0100

    added first PNG file implementation

--- sources/main.c
+++ sources/main.c
@@ -17,50 +17,56 @@ static const unsigned int kFlagHeight = 32;
 
 int main(int argc, char *argv[])
 {
-        unsigned char *image = malloc(kWidth * kHeight * 3);
+    unsigned char *image = malloc(kWidth * kHeight * 3);
 
-        // Country flags
-        RenderFrenchFlag(image, kFlagWidth, kFlagHeight);
-        SaveImageTGA("result/flagFR.tga", image, kFlagWidth, kFlagHeight);
+    // Country flags
+    RenderFrenchFlag(image, kFlagWidth, kFlagHeight);
+    SaveImageTGA("result/flagFR.tga", image, kFlagWidth, kFlagHeight);
+    SaveImagePNG("result/flagFR.png", image, kFlagWidth, kFlagHeight);
 
-        RenderJapaneseFlag(image, kFlagWidth, kFlagHeight);
-        SaveImageTGA("result/flagJP.tga", image, kFlagWidth, kFlagHeight);
+    RenderJapaneseFlag(image, kFlagWidth, kFlagHeight);
+    SaveImageTGA("result/flagJP.tga", image, kFlagWidth, kFlagHeight);
+    SaveImagePNG("result/flagJP.png", image, kFlagWidth, kFlagHeight);
 
-        RenderAmericanFlag(image, kFlagWidth, kFlagHeight);
-        SaveImageTGA("result/flagUS.tga", image, kFlagWidth, kFlagHeight);
+    RenderAmericanFlag(image, kFlagWidth, kFlagHeight);
+    SaveImageTGA("result/flagUS.tga", image, kFlagWidth, kFlagHeight);
+    SaveImagePNG("result/flagUS.png", image, kFlagWidth, kFlagHeight);
 
 
-        // Fractals
-        FractalParameters params;
-	params.iterations = 500;
+    // Fractals
+    FractalParameters params;
+    params.iterations = 500;
 
-        // Mandelbrot set
-	params.mode = FractalModeMandelbrot;
-	params.xMin = -2.1; params.xMax = 0.93;
-	params.yMin = -1.33; params.yMax = 1.33;
- 
-        RenderFractal(params, image, kWidth, kHeight);
-        SaveImageTGA("result/mandelbrot.tga", image, kWidth, kHeight);
+    // Mandelbrot set
+    params.mode = FractalModeMandelbrot;
+    params.xMin = -2.1; params.xMax = 0.93;
+    params.yMin = -1.33; params.yMax = 1.33;
 
-        // Julia set
-	params.mode = FractalModeJulia;
-	params.xMin = -1.54; params.xMax = 1.54;
-	params.yMin = -1.17; params.yMax = 1.17;
-        params.cr = -0.7; params.ci = 0.27;
+    RenderFractal(params, image, kWidth, kHeight);
+    SaveImageTGA("result/mandelbrot.tga", image, kWidth, kHeight);
+    SaveImagePNG("result/mandelbrot.png", image, kWidth, kHeight);
 
-        RenderFractal(params, image, kWidth, kHeight);
-        SaveImageTGA("result/julia.tga", image, kWidth, kHeight);
+    // Julia set
+    params.mode = FractalModeJulia;
+    params.xMin = -1.54; params.xMax = 1.54;
+    params.yMin = -1.17; params.yMax = 1.17;
+    params.cr = -0.7; params.ci = 0.27;
 
-        // Julia set 2
-        params.mode = FractalModeJulia;
-        params.xMin = -1.44; params.xMax = 1.44;
-        params.yMin = -1.17; params.yMax = 1.17;
-        params.cr = -0.4; params.ci = 0.6;
+    RenderFractal(params, image, kWidth, kHeight);
+    SaveImageTGA("result/julia.tga", image, kWidth, kHeight);
+    SaveImagePNG("result/julia.png", image, kWidth, kHeight);
 
-        RenderFractal(params, image, kWidth, kHeight);
-        SaveImageTGA("result/julia2.tga", image, kWidth, kHeight);
+    // Julia set 2
+    params.mode = FractalModeJulia;
+    params.xMin = -1.44; params.xMax = 1.44;
+    params.yMin = -1.17; params.yMax = 1.17;
+    params.cr = -0.4; params.ci = 0.6;
 
-        free(image);
+    RenderFractal(params, image, kWidth, kHeight);
+    SaveImageTGA("result/julia2.tga", image, kWidth, kHeight);
+    SaveImagePNG("result/julia2.png", image, kWidth, kHeight);
 
-        return 0;
+    free(image);
+
+    return 0;
 }
---
+++ sources/tools/imagePNG.c
@@ -0,0 +1,178 @@
+// imagePNG.c
+//
+// written by Thomas CARTON
+//
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "image.h"
+
+
+struct pngout
+{
+    uint8_t output[64];   /* output buffer */
+    size_t nout;          /* number of bytes to output */
+
+    uint16_t w, h;        /* width, height */
+    size_t bpl;           /* bytes per line */
+    uint32_t crc32;       /* running CRC32 */
+    uint16_t s1, s2;      /* Adler CRC sums */
+    uint16_t i, j;        /* i : column, j : row */
+};
+
+
+/* CRC32 computation */
+static const uint32_t crc_table[16] = {
+    0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
+    0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
+    0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
+    0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
+};
+
+static uint32_t crc_update(uint32_t crc, uint8_t data)
+{
+    uint8_t tbl_idx;
+    tbl_idx = crc ^ (data >> (0 * 4));
+    crc = crc_table[tbl_idx & 0x0f] ^ (crc >> 4);
+    tbl_idx = crc ^ (data >> (1 * 4));
+    crc = crc_table[tbl_idx & 0x0f] ^ (crc >> 4);
+    return crc;
+}
+
+/* b8: Output byte n and update the CRC32 */
+static void b8(struct pngout *s, uint8_t n)
+{
+    s->output[s->nout++] = n;
+    s->crc32 = crc_update(s->crc32, n);
+}
+
+/* adler8: Output byte n and update the CRC32 and Adler CRC */
+static void adler8(struct pngout *s, uint8_t n)
+{
+    b8(s, n);
+    s->s1 = (s->s1 + n) % 65521;
+    s->s2 = (s->s2 + s->s1) % 65521;
+}
+
+/* be32: Output 32-bit n big-endian */
+static void be32(struct pngout *s, uint32_t n)
+{
+    b8(s, n >> 24);
+    b8(s, n >> 16);
+    b8(s, n >> 8);
+    b8(s, n);
+}
+
+/* le16: Output 16-bit n little-endian */
+static void le16(struct pngout *s, uint32_t n)
+{
+    b8(s, n);
+    b8(s, n >> 8);
+}
+
+/* b8s: Output an array of bytes */
+static void b8s(struct pngout *s, const uint8_t *b, size_t n)
+{
+    while (n--)
+        b8(s, *b++);
+}
+
+/* start_chunk: output the start of a standard PNG chunk */
+static void start_chunk(struct pngout *s, const char *typecode, uint32_t size)
+{
+    be32(s, size);
+    s->crc32 = 0xffffffffUL;
+    b8s(s, (const uint8_t*)typecode, 4);
+}
+
+/* end_chunk: output the CRC32 at the end of a PNG chunk */
+static void end_chunk(struct pngout *s)
+{
+    be32(s, 0xffffffffUL ^ s->crc32);
+}
+
+
+void pngout_start(struct pngout *s, uint16_t width, uint16_t height)
+{
+    s->w = width;
+    s->h = height;
+
+    s->nout = 0;
+
+    static uint8_t first[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+    b8s(s, first, sizeof(first));
+
+    start_chunk(s, "IHDR", 13);
+    be32(s, width);
+    be32(s, height);
+    static uint8_t rgb[5] = {8, 2, 0, 0, 0};
+    b8s(s, rgb, sizeof(rgb));
+    end_chunk(s);
+
+    s->bpl = 1 + 3 * width;               /* Bytes per line */
+    start_chunk(s, "IDAT", 2 + height * (5 + s->bpl) + 4);
+    b8(s, 0x78);                          /* Start DEFLATE blocks */
+    b8(s, 0x01);
+    s->i = 0;                             /* Pixel coordinate (0,0) */
+    s->j = 0;
+
+    s->s1 = 1;                            /* Seed the Adler CRC */
+    s->s2 = 0;
+}
+
+void pngout_rgb(struct pngout *s, uint8_t r, uint8_t g, uint8_t b)
+{
+    s->nout = 0;
+
+    if (s->i == 0) {                      /* Start a block for each line */
+        b8(s, (s->j + 1) == s->h);          /* 1 if last line, 0 otherwise */
+        le16(s, s->bpl);                    /* Bytes per line, and inverted */
+        le16(s, ~s->bpl);
+        adler8(s, 0);                       /* Filter: none */
+    }
+
+    adler8(s, r);                         /* The pixel data itself */
+    adler8(s, g);
+    adler8(s, b);
+    s->i++;
+
+    if (s->i == s->w) {                   /* End of line means end of block */
+        s->i = 0;
+        if (++s->j == s->h) {               /* Is this the last line? */
+            be32(s, (s->s2 << 16) + s->s1);   /* Append the Adler CRC */
+            end_chunk(s);                     /* End of the IDAT chunk */
+            start_chunk(s, "IEND", 0);        /* IEND chunk means end of file */
+            end_chunk(s);
+        }
+    }
+}
+
+
+
+void SaveImagePNG(char *filename, unsigned char *image, int width, int height)
+{
+    struct pngout s;
+
+    FILE *outputFile = fopen(filename, "w");
+    if (outputFile == NULL)
+        goto error;
+
+    pngout_start(&s, width, height);
+    fwrite(s.output, s.nout, 1, outputFile);
+
+    for (unsigned int k = 0; k < width * height * 3; k += 3)
+    {
+        pngout_rgb(&s, image[k + 0], image[k + 1], image[k + 2]);
+        fwrite(s.output, s.nout, 1, outputFile);
+    }
+
+    fclose(outputFile);
+    return;
+
+error:
+    fprintf(stderr, "Failed to write image file '%s'\n", filename);
+    exit(-1);
+}
